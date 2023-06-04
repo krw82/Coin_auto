7e1967555b139040601bb0b137d509fd8dde7d71
jung0220 <86577565+krw82@users.noreply.github.com>
2023-06-05 00:45:27 +0900

    krw

---
+++ src/main/java/com/example/demo/coin/TA/BasicTa.java
@@ -0,0 +1,15 @@
+package com.example.demo.coin.TA;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class BasicTa {
+
+
+    public double Ma(){
+        
+
+        return 0.0;
+    }
+    
+}
